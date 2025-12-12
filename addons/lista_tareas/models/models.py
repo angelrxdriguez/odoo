46b7d45bf681a96d74838a46f88454c1eeb6a33f
angelrxdriguez <114663786+angelrxdriguez@users.noreply.github.com>
2025-12-12 17:01:15 +0100

    ACTIVIDAD 1 HECHA

--- addons/lista_tareas/models/models.py
+++ addons/lista_tareas/models/models.py
@@ -1,19 +1,25 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-# Definimos el modelo de datos
 class ListaTareas(models.Model):
     _name = 'lista_tareas.lista_tareas'
     _description = 'Lista de tareas'
 
-    # Campos
     tarea = fields.Char(string="Tarea")
     prioridad = fields.Integer(string="Prioridad", default=0)
     urgente = fields.Boolean(string="Urgente", compute="_value_urgente", store=True)
     realizada = fields.Boolean(string="Realizada", default=False)
-    responsable = fields.Char(string="Responsable")  # ← NUEVO
+    responsable = fields.Char(string="Responsable")
 
-    # Cálculo: urgente si prioridad > 10
+    estado = fields.Selection(
+        [
+            ('todo', 'Por hacer'),
+            ('doing', 'Haciendo'),
+            ('done', 'Hecha'),
+        ],
+        string="Estado",
+        default='todo',
+    )
+
     @api.depends('prioridad')
     def _value_urgente(self):
         for record in self:
